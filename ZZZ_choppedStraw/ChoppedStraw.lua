e0415b868c612846b4e82b8db34ca73541169c27
webalizer-gt <webalizer-gt@users.noreply.github.com>
2016-10-04 14:31:17 +0200

    remove fruittypes

--- ZZZ_choppedStraw/ChoppedStraw.lua
+++ ZZZ_choppedStraw/ChoppedStraw.lua
@@ -11,114 +11,87 @@ function ChoppedStraw.prerequisitesPresent(specializations)
 end;
 
 function ChoppedStraw:load(xmlFile)
-	self.getAreas = SpecializationUtil.callSpecializationsFunction("self.getAreas");
-	self.wwMinMaxAreas = SpecializationUtil.callSpecializationsFunction("self.wwMinMaxAreas");
-	self.createCStrawArea = SpecializationUtil.callSpecializationsFunction("self.createCStrawArea");
-	self.setCStrawArea = SpecializationUtil.callSpecializationsFunction("self.setCStrawArea");
+	-- Only activate when chopped-straw "fruits" are available
+	if g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDSTRAW]
+	and g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDRAPE]
+	and g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDMAIZE]
+	then
+		self.getAreas = ChoppedStraw.getAreas;
+		self.wwMinMaxAreas = ChoppedStraw.wwMinMaxAreas;
+		self.createCStrawArea = ChoppedStraw.createCStrawArea;
+		self.setCStrawArea = ChoppedStraw.setCStrawArea;
 
-	self.getAreas = ChoppedStraw.getAreas;
-	self.wwMinMaxAreas = ChoppedStraw.wwMinMaxAreas;
-	self.createCStrawArea = ChoppedStraw.createCStrawArea;
-	self.setCStrawArea = ChoppedStraw.setCStrawArea;
+		if self.chopperToggleTime ~= nil then
+			self.chopperToggleTime = math.max(4000, self.chopperToggleTime)
+		end;
 
-	if self.chopperToggleTime ~= nil then
-    if self.chopperToggleTime < 4000 then
-      self.chopperToggleTime = 4000;
-    end;
-  end;
+		-- Area creation
+		self.strawZOffset = -1.5;
+		self.strawNodeId = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.workAreas.workArea#startIndex"));
 
-	-- Area creation
-	self.strawZOffset = -1.5;
-	self.strawNodeId = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.workAreas.workArea#startIndex"));
-
-	if self.strawNodeId ~= nil then
 		self.cStrawAreas = {}
-		self.cStrawAreas = self:createCStrawArea();
-	end;
+		if self.strawNodeId ~= nil then
+			self.cStrawAreas = self:createCStrawArea();
+		end;
+	else
+		-- Disable chopped-straw for this vehicle.
+		self.strawNodeId = nil;
+	end
 end;
 
 function ChoppedStraw:delete()
 end;
 
 function ChoppedStraw:readStream(streamId, connection)
-
 end;
 
 function ChoppedStraw:writeStream(streamId, connection)
-
 end;
 
 function ChoppedStraw:mouseEvent(posX, posY, isDown, isUp, button)
-
 end;
 
 function ChoppedStraw:keyEvent(unicode, sym, modifier, isDown)
 end;
 
 function ChoppedStraw:onLeave()
-
 end
 
 function ChoppedStraw:update(dt)
-
 end;
 
 function ChoppedStraw:updateTick(dt)
-	if self.strawNodeId ~= nil then
-		if not self.isStrawActive then --or not fruitDesc.hasWindrow then
-			local preparingOutputId = nil;
+	if not self.isStrawActive
+	and self.strawNodeId ~= nil
+	and self.lastValidInputFruitType ~= FruitUtil.FRUITTYPE_UNKNOWN
+	then
+		local fruitDesc = FruitUtil.fruitIndexToDesc[self.lastValidInputFruitType];
+    local choppedStrawBinding = ChoppedStraw.strawBindings[fruitDesc.name];
 
-			if g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDSTRAW] and g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDRAPE] and g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDMAIZE] then
-        if self.isTurnedOn and self.movingDirection > 0 and self.lastValidInputFruitType ~= 0 then
-          local fruitDesc = FruitUtil.fruitIndexToDesc[self.lastValidInputFruitType];
-          if fruitDesc.name == "maize" then
-            preparingOutputId = g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDMAIZE].preparingOutputId;
-            for nIndex,oImplement in pairs(self.attachedImplements) do --parse all implements
-              if oImplement ~= nil and oImplement.object ~= nil then
-                if oImplement.object.threshingParticleSystems ~= nil and oImplement.object.threshingParticleSystems.isEmitting then
-                  for _,cuttingArea in pairs(oImplement.object.workAreas) do
-                    x, y, z = getWorldTranslation(cuttingArea.start)
-                    x1, y1, z1 = getWorldTranslation(cuttingArea.width)
-                    x2, y2, z2 = getWorldTranslation(cuttingArea.height)
-                    Utils.updateStrawHaulmArea(preparingOutputId, x, z, x1, z1, x2, z2)
-                  end;
-                end;
-              end;
-            end;
-          end;
-        end;
-
-        --[[if self.lastArea > 0 then
-					local fruitDesc = FruitUtil.fruitIndexToDesc[self.lastValidInputFruitType];
-					if fruitDesc.name == "maize" then
-						preparingOutputId = g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDMAIZE].preparingOutputId;
-						for nIndex,oImplement in pairs(self.attachedImplements) do --parse all implements
-							if oImplement ~= nil and oImplement.object ~= nil then
-								for _,cuttingArea in pairs(oImplement.object.workAreas) do
-									x, y, z = getWorldTranslation(cuttingArea.start)
-									x1, y1, z1 = getWorldTranslation(cuttingArea.width)
-									x2, y2, z2 = getWorldTranslation(cuttingArea.height)
-									Utils.updateStrawHaulmArea(preparingOutputId, x, z, x1, z1, x2, z2)
-								end;
-							end;
+		if self.isTurnedOn
+		and self.movingDirection > 0
+		and fruitDesc ~= nil
+		and choppedStrawBinding.strawOutputFront
+		then
+			for _,oImplement in pairs(self.attachedImplements) do --parse all implements
+				if oImplement ~= nil and oImplement.object ~= nil then
+					if oImplement.object.threshingParticleSystems ~= nil and oImplement.object.threshingParticleSystems.isEmitting then
+						for _,workArea in pairs(oImplement.object.workAreas) do
+							local x, _, z = getWorldTranslation(workArea.start)
+							local x1, _, z1 = getWorldTranslation(workArea.width)
+							local x2, _, z2 = getWorldTranslation(workArea.height)
+							Utils.updateStrawHaulmArea(choppedStrawBinding.strawTypeFoliageId, x, z, x1, z1, x2, z2)
 						end;
 					end;
-				end;]]--
-				if self.chopperPSenabled then
-					local fruitDesc = FruitUtil.fruitIndexToDesc[self.lastValidInputFruitType];
-					if fruitDesc.name == "rape" then
-						preparingOutputId = g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDRAPE].preparingOutputId;
-					elseif fruitDesc.name ~= "rape" and fruitDesc.name ~= "maize" then
-						preparingOutputId = g_currentMission.fruits[FruitUtil.FRUITTYPE_CHOPPEDSTRAW].preparingOutputId;
-					end;
-					if fruitDesc.name ~= "maize" then
-						for i = 1, table.getn(self.cStrawAreas) do
-							local x, y, z = getWorldTranslation(self.cStrawAreas[i].start)
-							local x1, y1, z1 = getWorldTranslation(self.cStrawAreas[i].width)
-							local x2, y2, z2 = getWorldTranslation(self.cStrawAreas[i].height)
-							Utils.updateStrawHaulmArea(preparingOutputId, x, z, x1, z1, x2, z2)
-						end;
-					end;
+				end;
+			end;
+		elseif self.chopperPSenabled then
+			if choppedStrawFoliageId ~= nil then
+				for i = 1, table.getn(self.cStrawAreas) do
+					local x, _, z = getWorldTranslation(self.cStrawAreas[i].start)
+					local x1, _, z1 = getWorldTranslation(self.cStrawAreas[i].width)
+					local x2, _, z2 = getWorldTranslation(self.cStrawAreas[i].height)
+					Utils.updateStrawHaulmArea(choppedStrawBinding.strawTypeFoliageId, x, z, x1, z1, x2, z2)
 				end;
 			end;
 		end;
@@ -162,7 +135,6 @@ function ChoppedStraw:wwMinMaxAreas(self,areas)
 	local maxA = 0;
 	if areas ~= nil then
 		for _,cuttingArea in pairs(areas) do
-
 				local x1,y1,z1 = getWorldTranslation(cuttingArea.start)
 				local x2,y2,z2 = getWorldTranslation(cuttingArea.width)
 				local x3,y3,z3 = getWorldTranslation(cuttingArea.height)
@@ -170,24 +142,8 @@ function ChoppedStraw:wwMinMaxAreas(self,areas)
 				local lx2,ly2,lz2 = worldToLocal(self.rootNode,x2,y2,z2)
 				local lx3,ly3,lz3 = worldToLocal(self.rootNode,x3,y3,z3)
 
-				if lx1 < minA then
-					minA = lx1;
-				end;
-				if lx1 > maxA then
-					maxA = lx1;
-				end;
-				if lx2 < minA then
-					minA = lx2;
-				end;
-				if lx2 > maxA then
-					maxA = lx2;
-				end;
-				if lx3 < minA then
-					minA = lx3;
-				end;
-				if lx3 > maxA then
-					maxA = lx3;
-				end;
+				minA = math.min(minA, lx1, lx2, lx3)
+				maxA = math.max(maxA, lx1, lx2, lx3)
 		end;
 	end;
 	return minA, maxA, ly1, lz1;
